020144472f2bb46418e6da177eb0cc686ecfa9c5
Yash Seeta <yashseeta94@gmail.com>
2021-04-21 21:41:43 +0200

    change home page to contain all the meme images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
+import Information from "../data/information.json";
+import MemeCard from "../component/MemeCard";
+import "../styles/Home.css"
+
 export default function Home() {
+
+    const MemeArray = Information.map((item) => (
+        <MemeCard key={item.id} data={item}/>
+    ))
     return(
-        <div>
-            SDA_MEMES
+        <div className="image-grid">
+            {MemeArray}
         </div>
     )
 }
