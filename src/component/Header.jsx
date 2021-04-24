e6df4a83889a301233d9b564b4950bad72141311
Yash Seeta <yashseeta94@gmail.com>
2021-04-24 20:41:41 +0200

    add links to home page

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,19 @@
+import {Link} from "react-router-dom";
+
 import "../styles/Header.css"
 
 export default function Header() {
-    return(
-        <header>SDA MEMES BY YASH</header>
-    )
+    return (
+        <div className="header-container">
+            <header className="logo">SDA MEMES BY YASH</header>
+            <div className="link-container">
+                <Link to="/" className="home">
+                    Home
+                </Link>
+                <Link to="/" className="home">
+                    Home
+                </Link>
+            </div>
+        </div>
+    );
 }
