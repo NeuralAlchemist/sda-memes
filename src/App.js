b11cfb76cae51cc7fd0deb746281fcded45d8a3a
Yash Seeta <yashseeta94@gmail.com>
2021-04-24 14:29:36 +0200

    Add header and global style for body

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 
+import "./styles/global.css"
+
 import Home from "./pages/Home";
+import Header from "./component/Header";
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Switch>
         <Route path="/">
           <Home/>
