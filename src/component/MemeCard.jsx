af9f863b45c899ae599764959e53dfa649aa7b82
Yash Seeta <yashseeta94@gmail.com>
2021-04-24 17:26:47 +0200

    embed link to each meme card

--- src/component/MemeCard.jsx
+++ src/component/MemeCard.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
 import "../styles/MemeCard.css";
 
 export default function MemeCard({data}) {
 
     const imageObject = require(`../images/${data.imageName}`);
     const imageActual = imageObject.default;
-    return(
+    return (
         <article className="MemeCard">
-            <img src={imageActual}>
-            </img>
+            <Link to={`meme/${data.imageName}`}>
+                <img src={imageActual}></img>
+            </Link>
         </article>
-    )
+    );
 };
