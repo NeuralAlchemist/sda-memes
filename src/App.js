536704b8b9f274db903ba9b4fea3ca0a21ede0c5
Yash Seeta <yashseeta94@gmail.com>
2021-05-02 21:27:57 +0200

    Remove meme extended

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter, Switch, Route} from "react-router-dom";
 import "./styles/global.css";
 import Home from "./pages/Home";
 import Header from "./component/Header";
-import MemeExtended from "./component/MemeExtended";
 import MemeSlider from "./component/MemeSlider";
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
         <Route path="/" exact>
           <Home/>
         </Route>
-        <Route path="/meme/:id" component={MemeExtended}></Route>
         <Route path="/slider" component={MemeSlider}/>
       </Switch>
     </BrowserRouter>
