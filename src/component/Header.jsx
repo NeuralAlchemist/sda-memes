aee22fdfd35c516e9878ed0bbb3c343a482c0df6
Yash Seeta <yashseeta94@gmail.com>
2021-05-02 15:36:05 +0200

    rename logo

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,7 +5,7 @@ import "../styles/Header.css"
 export default function Header() {
     return (
         <div className="header-container">
-            <header className="logo">SDA MEMES BY YASH</header>
+            <header className="logo">SDA MEMES </header>
             <div className="link-container">
                 <Link to="/" className="home">
                     Home
