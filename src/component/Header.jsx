bcb6d057e4abbb18d00b71039f7a98b5eb077266
Yash Seeta <yashseeta94@gmail.com>
2021-05-02 21:28:23 +0200

    redo header to make logo home

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,19 +1,16 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import "../styles/Header.css"
+import "../styles/Header.css";
 
 export default function Header() {
     return (
         <div className="header-container">
-            <header className="logo">SDA MEMES </header>
-            <div className="link-container">
-                <Link to="/" className="home">
-                    Home
-                </Link>
-                <Link to="/" className="home">
-                    Home
-                </Link>
-            </div>
+            <Link to="/" className="logo">
+                SDA MEMES
+            </Link>
+            <Link to="/" className="home">
+                About
+            </Link>
         </div>
     );
 }
