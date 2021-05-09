cc1b116b507009b8a3d0a6f06bfcd6ff32fe8704
Yash Seeta <yashseeta94@gmail.com>
2021-05-09 22:40:44 +0200

    add path and skeleton about compoenent

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./styles/global.css";
 import Home from "./pages/Home";
 import Header from "./component/Header";
 import MemeSlider from "./component/MemeSlider";
+import About from "./pages/About";
 function App() {
   return (
-    <BrowserRouter>
-    <Header/>
-      <Switch>
-        <Route path="/" exact>
-          <Home/>
-        </Route>
-        <Route path="/slider" component={MemeSlider}/>
-      </Switch>
-    </BrowserRouter>
+      <BrowserRouter>
+          <Header />
+          <Switch>
+              <Route path="/" exact>
+                  <Home />
+              </Route>
+              <Route path="/slider" component={MemeSlider} />
+              <Route path="/about" component={About}/>
+          </Switch>
+      </BrowserRouter>
   );
 }
 
