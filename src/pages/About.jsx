0559df6b98942b2e8f2def4131b2322b807998d0
Yash Seeta <yashseeta94@gmail.com>
2021-05-16 21:07:14 +0200

    add about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,11 @@
 export default function About() {
-    return(
-        <div>Something very meaningful</div>
-    )
+    return (
+      <div className="about">
+        <div className="message">
+          Hello there! This website exists to share all the memes made for
+          SDA9 by me and to remind every hardworking person to take a breather in times of struggle.{" "}
+        </div>
+        <div className="signature">- Yash</div>
+      </div>
+    );
 };
